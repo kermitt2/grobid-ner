2f06b0d6d9ebd51e353bc425035162746d0e3e65
Luca Foppiano <luca@foppiano.org>
2016-08-30 17:36:32 +0200

    Cleanup

--- grobid-ner/src/test/java/org/grobid/core/EngineMockTest.java
+++ grobid-ner/src/test/java/org/grobid/core/EngineMockTest.java
@@ -30,6 +30,11 @@ public abstract class EngineMockTest {
         engine = GrobidFactory.getInstance().createEngine();
     }
 
+    /**
+     * Try to get the GROBID_HOME from the environment variable or by using some default locations:
+     *  - ../grobid-home
+     *  - ../../grobid-home (in case the whole repository is cloned directly under the grobid project)
+     */
     public static void findGrobidHome() {
         String grobidHome = System.getenv("GROBID_HOME");
         if (!isEmpty(grobidHome)) {
